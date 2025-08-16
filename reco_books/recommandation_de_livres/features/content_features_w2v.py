0214293af8429a832978edb9c3869c90b0c11f83
redyddm <redouane.bouras2014@gmail.com>
2025-08-16 22:18:34 +0200

    creation features pour w2v

---
+++ reco_books/recommandation_de_livres/features/content_features_w2v.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+from loguru import logger
+from tqdm import tqdm
+import pandas as pd
+import gensim
+import typer
+
+from recommandation_de_livres.config import PROCESSED_DATA_DIR
+from recommandation_de_livres.iads.utils import save_df_to_csv, save_df_to_pickle
+
+app = typer.Typer()
+
+
+@app.command()
+def main(
+    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
+    input_path: Path = PROCESSED_DATA_DIR / "content_dataset.pkl",
+    output_path_csv: Path = PROCESSED_DATA_DIR / "features_w2v.csv",
+    output_path_pkl: Path = PROCESSED_DATA_DIR / "features_w2v.pkl",
+    # -----------------------------------------
+):
+    logger.info('Loading content dataset...')
+
+    content_df = pd.read_pickle(input_path)
+
+    logger.info('Fusionning the texts for Word2Vec...')
+
+    content_df['text_for_w2v'] = (
+        content_df['title'].fillna('') + ' ' +
+        content_df['categories'].fillna('') + ' ' +
+        content_df['authors'].fillna('') + ' ' +
+        content_df['description'].fillna('')
+    )
+
+    logger.info("Cleaning and tokenizing the text...")
+
+    tqdm.pandas(desc='Nettoyage des textes')
+    content_df['text_clean'] = content_df['text_for_w2v'].progress_apply(gensim.utils.simple_preprocess)
+
+    logger.info("Creating the features dataframe...")
+
+    features_df = pd.DataFrame({
+        'isbn': content_df['isbn'],
+        'text_clean': content_df['text_clean']
+    })
+
+    logger.info("Saving the features...")
+
+    save_df_to_csv(features_df, output_path_csv)
+    save_df_to_pickle(features_df, output_path_pkl)
+
+    logger.success("Word2Vec features generation complete.")
+    # -----------------------------------------
+
+
+if __name__ == "__main__":
+    app()
